4be52a79d3392a9dd62d08126418347e5360b559
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-11-13 10:02:53 +0100

    opslagstavle

--- Assets/Js/Modules/opslagstavle/opslagstavle.js
+++ Assets/Js/Modules/opslagstavle/opslagstavle.js
@@ -1,28 +1,6 @@
-
-// export default async function hentNyheder(){
-//     const url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.dr.dk%2Fnyheder%2Fservice%2Ffeeds%2Fallenyheder%23"
-//     res = await fetch(url);
-//     const data = await res.json();
-//     const parser = new DOMParser();
-//     const doc = parser.parseFromString(data.contents, "text/html" )
-
-//     //finder artikler(tilpas hvis deres struktur ændre sig)
-
-//     const artikler =doc.querySelectorAll("article")
-//     let html ="";
-//     artikler.forEach(a => {
-//         const titel = a.querySelector("h2, h3")
-//         const link = a.querySelector("a")?.href;
-//         if (titel && link) {
-//         html += `<p><a href= "${link}" target= "_blank">${titel}</a></p>`; 
-//         }
-//     })
-//     document.getElementById("opslagstavle").innerHTML = html || "Ingen nyheder fundet."; 
-// } 
-// hentNyheder();  
-
- // Lav en asynkron funktion (så vi kan bruge "await")
-    export default async function hentData() {
+//NYHEDER TIL OPSLAGSTAVLEN.(vi har brugt nyheder da vi ikke måtte bruge skolens)
+   
+export default async function hentData() {
       
         try {
         // Hent data fra et API
