5409de4c92a98f2df561b8bda08406685aa67dac
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-11-15 13:18:03 +0100

    new

--- Assets/Js/Modules/Ur/ur.js
+++ Assets/Js/Modules/Ur/ur.js
@@ -5,10 +5,10 @@ export default function updateClock(){
     const now = new Date();
     let hours = now.getHours();
     hours = hours % 12 || 12;
-    hours = hours.toString().padStart(2, 0);
+    hours = hours.toString().padStart(2,0);
     const minutes = now.getMinutes().toString().padStart(2,0);
-    const timeString = `${hours}:${minutes}`; 
-    document.getElementById("ur").textContent = timeString
+    const timeString = `${hours}: ${minutes}`; 
+    document.getElementById('ur').textContent = timeString
 
 }
 
